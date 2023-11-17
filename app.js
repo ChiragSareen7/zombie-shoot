de095447787962b21c2abc7d2cdaae12c4099c88
ChiragSareen7 <144792631+ChiragSareen7@users.noreply.github.com>
2023-11-17 18:05:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ const button = document.getElementById("button");
 button.onclick = () => {
   location.href = "./game.html";
 };
+
+// let gameBody=document.getElementById('game-body')
+// let lives = document.getElementById('lives')
+// let seconds=document.getElementById('timer').textContent
--- game.js
+++ game.js
@@ -17,3 +17,86 @@
 // Iteration 6: Write a code to start the game by calling the first zombie
 
 // Iteration 7: Write the helper function to get random integer
+let gameBody=document.getElementById('game-body')
+let lives=document.getElementById('lives')
+let seconds=document.getElementById('timer').textContent
+// console.log(seconds)
+let maxlives=4;
+
+let backgroundSound=new Audio('./assets/bgm.mp3')
+backgroundSound.play()
+backgroundSound.loop=true
+
+let shotgunSound=new Audio('./assets/shotgun.wav')
+shotgunSound.volume=0.1
+// shotgunSound.play()
+// shotgunSound.loop=true
+
+gameBody.onclick=()=>{
+    shotgunSound.pause()
+    shotgunSound.currentTime=0
+    shotgunSound.play()
+}
+var zombieId = 0;
+let img = [
+    "zombie-1.png",
+    "zombie-2.png",
+    "zombie-3.png",
+    "zombie-4.png",
+    "zombie-5.png",
+    "zombie-6.png",
+
+];
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+var zombieId=0;
+function create(){
+    z=img[getRandomInt(0,img.length)]
+    // console.log(z)
+    gameBody.innerHTML +=`<img src='./assets/${z}'
+    class="zombie-image" id="zombies${zombieId}">`;
+
+    let zombie=document.getElementById(`zombies${zombieId}`)
+    zombie.style.transform=`translateX(${getRandomInt(15,75)}vw)`
+    zombie.onclick=()=>{
+        zombieDestruct(zombie)
+    }
+}
+create()
+
+function checkCollision(zombie){
+    if(zombie.getBoundingClientRect().top<=0){
+        maxlives--
+        return true
+    }
+    return false
+}
+
+function zombieDestruct(zombie){
+    zombie.style.display='none';
+    zombieId++
+    create()
+}
+
+var timerId = setInterval(()=>{
+    seconds--
+    document.getElementById('timer').textContent=seconds
+    let zombie=document.getElementById(`zombies${zombieId}`)
+
+    if(checkCollision(zombie)==true){
+        zombieDestruct(zombie)
+        if(maxlives==0){
+            clearInterval(timerId)
+            location.href='./win.html'
+        }
+    }
+
+    if(seconds==0){
+        clearInterval(timerId)
+        location.href='./win.html'
+    }
+},1000)
